5c56875789ea26c48287907928579fae561e2b19
Johan Tell <johan@mintcore.se>
2023-10-28 20:58:20 +0200

    Add `SignatureStorage`

---
+++ lib/moxinet/signature_storage.ex
@@ -0,0 +1,54 @@
+defmodule Moxinet.SignatureStorage do
+  @moduledoc false
+
+  use GenServer
+
+  defmodule Signature do
+    defstruct [:mock_module, :pid, :method]
+  end
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, %{}, opts)
+  end
+
+  def init(args) do
+    {:ok, args}
+  end
+
+  def store(scope, method, callback, from_pid \\ self(), pid \\ __MODULE__) do
+    signature = %Signature{
+      mock_module: scope,
+      pid: from_pid,
+      method: method
+    }
+
+    GenServer.call(pid, {:store, signature, callback})
+  end
+
+  def find_signature(scope, from_pid, method, pid \\ __MODULE__) do
+    signature = %Signature{
+      mock_module: scope,
+      pid: from_pid,
+      method: method
+    }
+
+    GenServer.call(pid, {:find_signature, signature})
+  end
+
+  def handle_call({:store, %Signature{} = signature, callback}, _from, state) do
+    {:reply, :ok, Map.put(state, signature, callback)}
+  end
+
+  def handle_call({:find_signature, signature}, _from, state) do
+    response =
+      case Map.get(state, signature, :not_found) do
+        callback when is_function(callback) ->
+          {:ok, callback}
+
+        :not_found ->
+          {:error, :not_found}
+      end
+
+    {:reply, response, state}
+  end
+end
---
+++ test/moxinet/signature_storage_test.exs
@@ -0,0 +1,47 @@
+defmodule Moxinet.SignatureStorageTest do
+  use ExUnit.Case, async: true
+
+  alias Moxinet.SignatureStorage
+
+  describe "store/1" do
+    test "stores a signature in the storage" do
+      {:ok, storage_pid} = SignatureStorage.start_link([])
+      method = :post
+      test_pid = self()
+      callback = fn _, _ -> {:ok, []} end
+
+      :ok = SignatureStorage.store(__MODULE__, method, callback, test_pid, storage_pid)
+
+      assert %{
+               %SignatureStorage.Signature{
+                 mock_module: __MODULE__,
+                 pid: ^test_pid,
+                 method: ^method
+               } => ^callback
+             } = :sys.get_state(storage_pid)
+    end
+  end
+
+  describe "find_signature/3" do
+    test "finds a signature matching the passed arguments" do
+      {:ok, storage_pid} = SignatureStorage.start_link([])
+      method = :post
+      test_pid = self()
+      callback = fn _, _ -> {:ok, []} end
+
+      :ok = SignatureStorage.store(__MODULE__, method, callback, test_pid, storage_pid)
+
+      assert {:ok, ^callback} =
+               SignatureStorage.find_signature(__MODULE__, test_pid, method, storage_pid)
+    end
+
+    test "returns an error when the signature wasn't registered" do
+      {:ok, storage_pid} = SignatureStorage.start_link([])
+      method = :post
+      test_pid = self()
+
+      assert {:error, :not_found} =
+               SignatureStorage.find_signature(__MODULE__, test_pid, method, storage_pid)
+    end
+  end
+end
