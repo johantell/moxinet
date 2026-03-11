92e6c0eaf0186d1943769ae8a6b4478168585b3f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-03-11 13:12:18 +0100

    Add test check that runs mix test in nix sandbox
    Uses mixRelease with mixEnv="test" and a separate fetchMixDeps
    for test dependencies. Tests run as part of nix flake check.

--- flake.nix
+++ flake.nix
@@ -11,20 +11,23 @@
       pkgs = nixpkgs.legacyPackages.${system};
       beamPackages = pkgs.beam.packages.erlang;
       inherit (beamPackages) elixir hex;
+
+      version = "0.7.0";
+
+      mixFodDeps = beamPackages.fetchMixDeps {
+        pname = "mix-deps-moxinet";
+        inherit version;
+        src = ./.;
+        hash = "sha256-/eeLhmiFKs+ydXLatR2iO96sV+/nsiFKQmmdHaOFGas=";
+      };
     in
     {
       packages.${system} = {
         default = beamPackages.mixRelease {
           pname = "moxinet";
-          version = "0.7.0";
+          inherit version;
           src = ./.;
-
-          mixFodDeps = beamPackages.fetchMixDeps {
-            pname = "mix-deps-moxinet";
-            version = "0.7.0";
-            src = ./.;
-            hash = "sha256-/eeLhmiFKs+ydXLatR2iO96sV+/nsiFKQmmdHaOFGas=";
-          };
+          inherit mixFodDeps;
 
           meta = {
             description = "Mocking server that allows parallel testing over HTTP";
@@ -45,6 +48,43 @@
       checks.${system} = {
         inherit (self.packages.${system}) default;
         devShell = self.devShells.${system}.default;
+
+        tests = beamPackages.mixRelease {
+          pname = "moxinet-tests";
+          inherit version;
+          src = ./.;
+          mixEnv = "test";
+
+          mixFodDeps = beamPackages.fetchMixDeps {
+            pname = "mix-deps-moxinet-test";
+            inherit version;
+            src = ./.;
+            mixEnv = "test";
+            hash = "sha256-Sr+JSUSsQffpI0yKbgwZDSkl1DbubNT79ufmRbjshTU=";
+          };
+
+          # Run tests instead of building a release
+          buildPhase = ''
+            runHook preBuild
+            mix compile --no-deps-check
+            runHook postBuild
+          '';
+
+          checkPhase = ''
+            mix test --no-deps-check
+          '';
+          doCheck = true;
+
+          # We don't actually need the release output
+          installPhase = ''
+            runHook preInstall
+            touch $out
+            runHook postInstall
+          '';
+
+          # Skip the postFixup from mixRelease that looks for release files
+          postFixup = "";
+        };
       };
     };
 }
